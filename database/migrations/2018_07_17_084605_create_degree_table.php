a68c7feaa316fc4cc8162aabf3ddb23a4d4ad596
ParhamNikdouz <pnikdouz@gmail.com>
2018-07-17 13:26:17 +0430

    Add table columns to migration

--- database/migrations/2018_07_17_084605_create_degree_table.php
+++ database/migrations/2018_07_17_084605_create_degree_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDegreeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('degrees', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->binary('group_manager');
-            $table->tinyInteger('degree_id');
-            $table->rememberToken();
+            $table->string('title');
+            $table->integer('max_student');
             $table->timestamps();
             $table->engine = 'InnoDB';
             $table->charset = 'utf8';
@@ -35,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('degrees');
     }
 }
---
+++ database/migrations/2018_07_17_084634_create_student_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->text('title_project');
+            $table->integer('stu_number');
+            $table->integer('master_id');
+            $table->string('deadline')->nullable();
+            $table->integer('refree_id')->nullable();
+            $table->string('defence_situation')->nullable();
+            $table->string('complementary')->nullable();
+            $table->integer('term_id')->nullable();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
